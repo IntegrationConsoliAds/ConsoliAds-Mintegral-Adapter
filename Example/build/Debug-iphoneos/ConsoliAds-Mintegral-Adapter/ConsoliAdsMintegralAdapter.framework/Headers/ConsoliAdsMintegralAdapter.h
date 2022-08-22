ae4d496fb256b0cb284d5d3cf9f32aa656e13a79
Osama Nadeem <osama.nadeem@consoliads.com>
2022-08-22 12:17:17 +0500

    update config and version.

---
+++ Example/build/Debug-iphoneos/ConsoliAds-Mintegral-Adapter/ConsoliAdsMintegralAdapter.framework/Headers/ConsoliAdsMintegralAdapter.h
@@ -0,0 +1,19 @@
+//
+//  ConsoliAdsMintegralAdapter.h
+//  ConsoliAdsMintegralAdapter
+//
+//  Created by OsamaNadeem on 1/3/22.
+//  Copyright © 2022 ConsoliAds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConsoliAdsMintegralAdapter.
+FOUNDATION_EXPORT double ConsoliAdsMintegralAdapterVersionNumber;
+
+//! Project version string for ConsoliAdsMintegralAdapter.
+FOUNDATION_EXPORT const unsigned char ConsoliAdsMintegralAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConsoliAdsMintegralAdapter/PublicHeader.h>
+
+
---
+++ Example/build/Pods.build/Debug-iphoneos/ConsoliAds-Mintegral-Adapter.build/DerivedSources/ConsoliAds_Mintegral_Adapter_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ConsoliAds_Mintegral_AdapterVersionString[];
+ extern const double ConsoliAds_Mintegral_AdapterVersionNumber;
+
+ const unsigned char ConsoliAds_Mintegral_AdapterVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ConsoliAds_Mintegral_Adapter  PROJECT:Pods-1" "\n";
+ const double ConsoliAds_Mintegral_AdapterVersionNumber __attribute__ ((used)) = (double)1.;
